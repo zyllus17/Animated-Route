e9cee3b773df3fa0eec8599929a0a5c473776dfc
Maruf Hassan <mrfhassan17@gmail.com>
2020-07-11 16:31:41 +0530

    Redirects to HomePage

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:animated_map/screens/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Maps',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
